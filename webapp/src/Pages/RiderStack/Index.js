2a5611e85e1a0ae2623e6c270c4641e84b9981c1
Varendra Maurya <varendramaurya@gmail.com>
2023-02-06 21:21:49 +0530

    fixed routing

--- webapp/src/Pages/RiderStack/Index.js
+++ webapp/src/Pages/RiderStack/Index.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import ReactBingmaps from 'bingmaps-react';
+import { ReactBingmaps } from 'react-bingmaps';
+// import ReactBingmaps from 'bingmaps-react';
 import classes from './Index.module.css';
 
 const Rider = () => {
@@ -12,6 +13,23 @@ const Rider = () => {
 				bingmapKey={
 					'AtD6KKbxZbMGumtiusZaHBClfullYMvlqCbIacNNkQQu-ONLx-95xel_a6y45wTH'
 				}
+				directions={{
+					requestOptions: { routeMode: 'driving', maxRoutes: 2 },
+					wayPoints: [
+						{
+							address: 'Allahabad, Uttar Pradesh',
+							location: [25.4358, 81.8463],
+						},
+						{
+							address: 'Bengaluru, Karnataka',
+							location: [12.9716, 77.5946],
+						},
+						{
+							address: 'Dharwad, Karnataka',
+							location: [15.531758, 74.936173],
+						},
+					],
+				}}
 				// directions={this.state.directions}
 			></ReactBingmaps>
 		</div>
